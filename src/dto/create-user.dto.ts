c7e16edcb4f7a75962d7aa45cad4d05dfac34823
Anoop017 <anoopsnair1123@gmail.com>
2026-06-09 21:49:52 +0530

    Testing to see what happens if i push to a merged branch

--- src/dto/create-user.dto.ts
+++ src/dto/create-user.dto.ts
@@ -1,8 +1,10 @@
 import { IsString, IsNotEmpty } from "class-validator";
+import { userRoles } from "src/users/user-role.enum";
 
 export class CreateUserDto{
     @IsString()
     @IsNotEmpty()
     name!:string;
+    role!:string;
 
 }
